347e3e6c91ef77481303ac8221f67268372cdf65
kenobitsu7 <franciagbo@hotmail.fr>
2025-07-01 12:01:06 +0200

    5

---
+++ Coin_3D/Assets/Scripts/EnemyKill.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyKill : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var enemy = other.gameObject.GetComponent<EnemyController>();
+            enemy.Kill();
+        }
+    }
+}
