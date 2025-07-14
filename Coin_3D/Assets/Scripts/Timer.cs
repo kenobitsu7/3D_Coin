b1f44387994a23aacfdd129afec3eabbcde61f70
kenobitsu7 <franciagbo@hotmail.fr>
2025-07-14 13:20:10 +0200

    8

---
+++ Coin_3D/Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float timeValue = 90;
+
+    public GameObject GameOver;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
+    }
+    void DisplayTime(float timeToDisplay)
+    { 
+        if (timeToDisplay < 0)
+        { 
+            timeToDisplay = 0;
+
+            GameOver.gameObject.SetActive(true);
+            Time.timeScale = 0;
+
+        }
+        else if (timeToDisplay > 0)
+        {
+            timeToDisplay += 1;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
