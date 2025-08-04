ee7f8fc229179c7753fcf71327a99c15827b0664
kenobitsu7 <franciagbo@hotmail.fr>
2025-08-04 17:58:30 +0200

    Safe

--- Coin_3D/Assets/Scripts/GameManager.cs
+++ Coin_3D/Assets/Scripts/GameManager.cs
@@ -6,17 +6,18 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    private int score;
-    
+    private int score; 
     public int Score {  get { return score; } set { score = value; } }
 
     private bool timerIsStop;
     public bool TimerIsStop { get { return timerIsStop; } set { timerIsStop = value; } }
+    
     private float time;
     public float Time { get {return time; } set { time = value; } }
 
     private int killCount;
     public int KillCount { get { return killCount; } set { killCount = value; } }
+    
     private void Awake()
     {
         if (Instance == null)
--- Coin_3D/Assets/Scripts/MenuReturn.cs
+++ Coin_3D/Assets/Scripts/MenuReturn.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuReturn : MonoBehaviour
-{    
+{
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {           
             SceneManager.LoadScene("MainMenu");
+
+            GameManager.Instance.Score = 0;
+            GameManager.Instance.KillCount = 0;
+
+            GameManager.Instance.TimerIsStop = false;
+            GameManager.Instance.Time = 90f;
         }
     }
 }
